fe8eb6a532ac14b910edf526c77ab85781fef77a
Mark Herzog <111207532+MarkHerzog@users.noreply.github.com>
2022-08-19 17:43:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from population import Population
 from game import Game
+import matplotlib.pyplot as plt
 
 pop = Population()
 game = Game()
+gen =[]
+max_fitness = []
+plt.xlabel('Gen')
+plt.ylabel('Max_fitness')
 while True:
     pop.dead(game.play_frame(pop.birds, pop))
     for bird in pop.birds:
          bird.update([game.close_pillar().y2 / 10, bird.y / 10, bird.y_change / 10], game)
     if len(pop.birds) == 0:
         game.restart()
-        pop.new()
+        max_fitness.append(pop.new() / 100)
+        gen.append(pop.gen)
+    plt.plot(gen, max_fitness, 'ko--')
